715d08131c7f569febe82ba06f03a84908c43eac
Alexey Roik <alexey.roik@nebulagames.org>
2020-04-01 00:54:53 +0300

    Add macOS, tvOS and watchOS targets

---
+++ ZenSpriteKit-tvOS/ZenSpriteKit_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  ZenSpriteKit_tvOS.h
+//  ZenSpriteKit-tvOS
+//
+//  Created by Alexey Roik on 4/1/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenSpriteKit_tvOS.
+FOUNDATION_EXPORT double ZenSpriteKit_tvOSVersionNumber;
+
+//! Project version string for ZenSpriteKit_tvOS.
+FOUNDATION_EXPORT const unsigned char ZenSpriteKit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenSpriteKit_tvOS/PublicHeader.h>
+
+
---
+++ ZenSpriteKit-watchOS/ZenSpriteKit_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  ZenSpriteKit_watchOS.h
+//  ZenSpriteKit-watchOS
+//
+//  Created by Alexey Roik on 4/1/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenSpriteKit_watchOS.
+FOUNDATION_EXPORT double ZenSpriteKit_watchOSVersionNumber;
+
+//! Project version string for ZenSpriteKit_watchOS.
+FOUNDATION_EXPORT const unsigned char ZenSpriteKit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenSpriteKit_watchOS/PublicHeader.h>
+
+
