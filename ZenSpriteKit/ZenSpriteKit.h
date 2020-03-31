4df8052107b716fe0737e4f7f1cd0f4d78eb636f
Alexey Roik <alexey.roik@nebulagames.org>
2020-04-01 00:41:14 +0300

    Add empty 'Swift' project

---
+++ ZenSpriteKit/ZenSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  ZenSpriteKit.h
+//  ZenSpriteKit
+//
+//  Created by Alexey Roik on 4/1/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenSpriteKit.
+FOUNDATION_EXPORT double ZenSpriteKitVersionNumber;
+
+//! Project version string for ZenSpriteKit.
+FOUNDATION_EXPORT const unsigned char ZenSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenSpriteKit/PublicHeader.h>
+
+
