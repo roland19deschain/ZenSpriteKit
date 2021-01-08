d2bda3ec69d468595b264e8d5626bbdaa4b6ee69
Alexey Roik <alexey.roik@nebulagames.org>
2021-01-08 03:20:23 +0300

    Add 'SKNode+Action' extension

---
+++ ZenSpriteKit/SourceCode/Extensions/SpriteKit/Node/SKNode+Action.swift
@@ -0,0 +1,16 @@
+import SpriteKit
+
+extension SKNode {
+	
+	/// Run action only if there is no active action for the same key.
+	func tryRun(
+		_ anAction: SKAction,
+		withKey key: String
+	) {
+		guard action(forKey: key) == nil else {
+			return
+		}
+		run(anAction, withKey: key)
+	}
+	
+}
